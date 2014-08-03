9622e1ae7abdc56ea989e9096c6729bb12302c53
Laguna <laguna_1989@gmx.net>
2014-08-03 18:32:17 +0200

    adding pixelgrid and movementscript

---
+++ Assets/Scripts/PixelGridScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelGridScript : MonoBehaviour {
+
+    private float cell_size = 1.0f/16.0f; 
+    private float x, y, z;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// coerce position to pixel grid
+	void Update () 
+    {
+        x = Mathf.Round(transform.position.x / cell_size) * cell_size;
+        y = Mathf.Round(transform.position.y / cell_size) * cell_size;
+        z = transform.position.z;
+        transform.position = new Vector3(x, y, z);
+	}
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementController : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            MoveRight();
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            MoveLeft();
+        }
+	}
+
+    private void MoveRight()
+    {
+        this.transform.position = new Vector3(this.transform.position.x + 0.25f, this.transform.position.y);
+    }
+
+    private void MoveLeft()
+    {
+        this.transform.position = new Vector3(this.transform.position.x - 0.25f, this.transform.position.y);
+    }
+}
