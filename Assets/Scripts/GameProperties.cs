b1fe4a0e1cc52e69f31b1b8051f5ba447bbe7269
Thunraz <julian.dinges@gmail.com>
2014-08-04 21:57:16 +0200

    Enemy is now pushed back instead of repositioned

--- Assets/Scripts/GameProperties.cs
+++ Assets/Scripts/GameProperties.cs
@@ -1,8 +1,8 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public static class GameProperties 
+﻿
+public static class GameProperties
 {
-    public static float PlayerMoveForce   {get {return 200f;}}
+    public static float PlayerMoveForce { get { return 200f; } }
     public static float PlayerMaxVelocity { get { return 0.5f; } }
+
+    public static float EnemyKnockbackForce { get { return 400f; } }
 }
